ab18645ea2837f1c7e9ab2defdc3ea9fb155a207
Piotr Martynski <piotr.martynski@mesh-systems.com>
2023-01-10 14:50:38 +0100

    Outdated script removed

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-terraform -chdir=./infrastructure plan -out=plan
-terraform -chdir=./infrastructure apply 'plan'
-
-terraform -chdir=./deploy plan -out=plan
-terraform -chdir=./deploy apply 'plan'
